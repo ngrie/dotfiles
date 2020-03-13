64cbf85fd693d6dc9dcec0aa16097017b532930d
Niklas Grießer <niklas@griesser.me>
2020-03-13 22:19:17 +0100

    Add NVM integration

--- exports.zsh
+++ exports.zsh
@@ -1,3 +1,8 @@
 export PATH="$HOME/.bin:$HOME/.composer/vendor/bin:$PATH"
 
 export EDITOR="vim"
+
+# NVM integration
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
