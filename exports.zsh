20ee4f318bb0eda6584f4c85c6171182938a3fc6
Niklas Grießer <niklas@griesser.me>
2024-06-29 14:57:24 +0200

    Update exports.zsh

--- exports.zsh
+++ exports.zsh
@@ -2,7 +2,6 @@ export PATH="$HOME/.bin:$HOME/.composer/vendor/bin:$PATH"
 
 export EDITOR="vim"
 
-# NVM integration
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+# Volta.sh integration (install by running "curl https://get.volta.sh | bash -s -- --skip-setup")
+export VOLTA_HOME="$HOME/.volta"
+export PATH="$VOLTA_HOME/bin:$PATH"
